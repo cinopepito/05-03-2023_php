3ffa44f4926739d4d5943a25589b09f1bcf65f68
cinopepito <83885361+cinopepito@users.noreply.github.com>
2023-03-05 20:33:39 +0100

    third commit

---
+++ number.php
@@ -0,0 +1,9 @@
+<?php
+echo pow(2,4).'<br>';
+echo sqrt(100).'<br>';
+echo max(100,200).'<br>';
+echo min(1,45).'<br>';
+echo round(3.27).'<br>';
+echo ceil(2.356).'<br>';
+echo floor(3.45).'<br>';
+?>
